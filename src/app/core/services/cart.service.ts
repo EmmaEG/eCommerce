f1f043adca569e238be454fd935e5bba42598cec
Emmanuel Granara <emanuelgranara@hotmail.com>
2020-08-07 09:01:17 -0300

    comentarios

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -16,21 +16,26 @@ export class CartService {
 
   constructor() { }
 
-  addCart(product: Product) { // metodo para añadir al carrtio
-    this.products = [...this.products, product]; // 4
-    this.cart.next(this.products); // 5
+  addCart(product: Product) { // 4 metodo para añadir al carrtio
+    this.products = [...this.products, product]; // 5
+    this.cart.next(this.products); // 6
   }
 
 }
 
 
 // 1 declaramos una variable products que es un array vacío de tipo product array
-/* 2 una instancia de behavior de tipo array de productos, es decir que controle el flujo de datos de este
-array y lo inicializamos en vacío*/
-/* 3 declaramos una variable publica para que cualquiera se subscriba a ella y tener los cambios en tiempo
-real */
-/* 4 cada vez que agreguen algo al carrito inserto una nueva variable, sin embargo no lo hago con el metodo
+/* 2 creamos la variable cart que será una instancia de BehaviorSubject, y le decimos que es de tipo array de
+productos, es decir que controle el flujo de datos de este array de productos "<Product[]>" y lo inicializamos
+en un array en vacío, es decir el carrito inicia con cero productos*/
+/* 3 declaramos una variable publica que pueda ser preguntada por cualquier componente y que sea de tipo observable
+para que cualquiera se subscriba a ella y tener los cambios en tiempo real, entonces simplemente la instanciamos
+"this.cart" y le decimos que se comporte como un observable "".asObservable"*/
+
+/*6 ahora creamos un metodo para agregar productos al carrito, a este metodo le enviamos un producto (obvio)
+que es de tipo product */
+/* 5 cada vez que agreguen algo al carrito inserto una nueva variable, sin embargo no lo hago con el metodo
 push xq cada vez voy a hacer una nueva referencia del arreglo y luego de la coma agregamos el último
 producto, de esta manera estamos generando un nuevo estado del array */
-/* 5  luego notificamos a todos los componentes que esten subscriptos que algo se agregó al carrito, estso
-lo hago con next y luego le paso la copia del estado actual*/
+/* 6  luego notificamos a todos los componentes que esten suscriptos que algo se agregó al carrito, estso
+lo hago con next y luego le paso la copia del array o del nuevo estado actual "this.products"*/
